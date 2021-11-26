32217c2ca2c8659c9870e338d3e6ceb0bdef88ee
MrRiazAhmed <mail.ahmedriaz@gmail.com>
2021-11-26 23:59:12 +0600

    hi

--- src/Firebase/firebase.init.js
+++ src/Firebase/firebase.init.js
@@ -2,7 +2,6 @@ import { initializeApp } from "firebase/app";
 import firebaseConfig from "./firebase.config";
 
 
-
 const initializeAuthentication = () => {
     initializeApp(firebaseConfig);
 };
--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -12,3 +12,70 @@ const NotFound = () => {
 };
 
 export default NotFound;
+
+import { Switch, Route, Link, useRouteMatch } from "react-router-dom";
+import Review from '../Review/Review';
+import MyOrders from '../MyOrders/MyOrders';
+import MakeAdmin from '../MakeAdmin/MakeAdmin';
+import AddProduct from '../AddProduct/AddProduct';
+import ManageProducts from '../ManageProducts/ManageProducts';
+
+let { path, url } = useRouteMatch();
+
+<div className="dashboard-container ">
+<div className="row">
+  <div className="col-md-3 ">
+    <div className="dashboard">
+      <h5>Dashboard</h5>
+      <Link to={`${url}`}>
+        <li className="dashboard-menu mt-5">DashBoard</li>
+      </Link>
+
+      <Link to={`${url}/myorders`}>
+        <li className="dashboard-menu mt-5">My Orders</li>
+      </Link>
+
+      <Link to={`${url}/review`}>
+        <li className="dashboard-menu mt-5">Review</li>
+      </Link>
+      <div className="admin-dashboard">
+        <li className="dashboard-menu mt-5">Orders list</li>
+
+       
+          <Link to={`${url}/addproduct`}>
+            <li className="dashboard-menu">Add Product</li>
+          </Link>
+        
+        <Link to={`${url}/makeadmin`}>
+          <li className="dashboard-menu">Make Admin</li>
+        </Link>
+        <Link to={`${url}/manageproducts`}>
+          <li className="dashboard-menu">Manage Products</li>
+        </Link>
+      </div>
+    </div>
+  </div>
+  <div className="col-md-9">
+    <Switch>
+      <Route exact path={path}>
+        <Dashboard></Dashboard>
+      </Route>
+      <Route exact path={`${path}/review`}>
+        <Review></Review>
+      </Route>
+      <Route exact path={`${path}/myorders`}>
+        <MyOrders></MyOrders>
+      </Route>
+      <Route exact path={`${path}/makeadmin`}>
+       <MakeAdmin></MakeAdmin>
+      </Route>
+      <Route exact path={`${path}/addproduct`}>
+        <AddProduct></AddProduct>
+      </Route>
+      <Route exact path={`${path}/manageproducts`}>
+        <ManageProducts></ManageProducts>
+      </Route>
+    </Switch>
+  </div>
+</div>
+</div>
